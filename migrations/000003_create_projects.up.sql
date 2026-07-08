ffbe0c6c9dbd256a87a594eee7e89520198012ab
Mahdi Alani <Themahdi67@gmail.com>
2026-07-08 19:01:01 -0400

    Fixed issues with autogenerated project migrations

--- migrations/000003_create_projects.up.sql
+++ migrations/000003_create_projects.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE projects (
     id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     workspace_id UUID NOT NULL REFERENCES workspaces(id) ON DELETE CASCADE,
     name         TEXT NOT NULL,
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_projects_workspace_id ON projects(workspace_id);
