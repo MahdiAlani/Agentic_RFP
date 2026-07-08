17b2735b7d23200f65d3b08f0cf5013760affcda
Mahdi Alani <Themahdi67@gmail.com>
2026-07-08 18:55:33 -0400

    Removed updated_at column from projects

--- migrations/000003_create_projects.up.sql
+++ migrations/000003_create_projects.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE projects (
     workspace_id UUID NOT NULL REFERENCES workspaces(id) ON DELETE CASCADE,
     name         TEXT NOT NULL,
     created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_projects_workspace_id ON projects(workspace_id);
